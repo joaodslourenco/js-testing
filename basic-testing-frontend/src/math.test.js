867b12e966b6d8b615665a936eddbd10a42f0260
Joao Lourenco <joaodslourenco@gmail.com>
2023-06-05 08:35:04 -0300

    add AAA

--- basic-testing-frontend/src/math.test.js
+++ basic-testing-frontend/src/math.test.js
@@ -2,6 +2,13 @@ import { expect, it } from 'vitest'
 import { add } from './math'
 
 it('should summarize all numbers in an array', () => {
-    const results = add([1, 2, 3])
-    expect(results).toBe(6)
+    // Arrange
+    const numbers = [1, 2, 3]
+    const expectedResult = numbers.reduce((prevValue, curValue) => prevValue + curValue, 0)
+
+    // Act
+    const results = add(numbers)
+
+    // Assert
+    expect(results).toBe(expectedResult)
 })
